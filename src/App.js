efd619b5fdd1a72f82a79f9b8c5f721d285512b6
Fred <frederic.Flaceliere@gmail.com>
2022-04-07 15:24:01 +0200

    update import

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import {
     useMantineTheme,
 } from '@mantine/core';
 
-import Home from "./containers/Home/Home";
-import Connexion from "./containers/Connexion/Connexion";
+import Home from "./pages/Home/Home";
+import Connexion from "./pages/Connexion/Connexion";
 
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
-import SignUp from "./containers/SignUp/SignUp";
-import Navbar from './components/Navbar/Navbar';
+import SignUp from "./pages/SignUp/SignUp";
+import Navbar from './components/Navbar/NavbarHead';
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./services/UserService";
 import { useKeycloak } from "@react-keycloak/web";
-import Welcome from './containers/Welcome/Welcome';
+import Welcome from './pages/Welcome/Welcome';
 
 function App() {
     const theme = useMantineTheme();
@@ -29,13 +29,13 @@ function App() {
             <div>
                 <Router>
                     <Switch>
-                        {!keycloak.authenticated && 
+                        {!keycloak.authenticated &&
                             <Route exact path='/' component={Welcome} />
                         }
                         {!!keycloak.authenticated &&
                             <Route exact path='/' component={Home} />
                         }
-                        
+
                         <Route exact path='/connexion' component={Connexion} />
                         <Route exact path='/signup' component={SignUp} />
                     </Switch>
@@ -43,7 +43,7 @@ function App() {
             </div>
 
         </ReactKeycloakProvider>
-        
+
         </>
     );
 }
