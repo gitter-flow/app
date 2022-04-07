2b791f164707527a73279f79c94f618062604e4e
paulb <barrie.paul@protonmail.com>
2022-04-07 19:01:46 +0200

    fix: keycloak

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ import {
 } from '@mantine/core';
 
 import Home from "./containers/Home/Home";
-import Connexion from "./containers/Connexion/Connexion";
 
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
-import SignUp from "./containers/SignUp/SignUp";
-import Navbar from './components/Navbar/Navbar';
 import { ReactKeycloakProvider } from "@react-keycloak/web";
-import keycloak from "./services/UserService";
-import { useKeycloak } from "@react-keycloak/web";
 import Welcome from './containers/Welcome/Welcome';
+import keycloak from './services/Keycloak';
+import PrivateRoute from './helpers/PrivateRoute';
 
 function App() {
     const theme = useMantineTheme();
@@ -25,19 +22,11 @@ function App() {
   return (
       <>
         <ReactKeycloakProvider authClient={keycloak}>
-            {keycloak.authenticated && <Navbar/>}
             <div>
                 <Router>
                     <Switch>
-                        {!keycloak.authenticated && 
-                            <Route exact path='/' component={Welcome} />
-                        }
-                        {!!keycloak.authenticated &&
-                            <Route exact path='/' component={Home} />
-                        }
-                        
-                        <Route exact path='/connexion' component={Connexion} />
-                        <Route exact path='/signup' component={SignUp} />
+                        <Route  path='/home' component={Welcome} />
+                        <PrivateRoute  path='/' component={Home} />
                     </Switch>
                 </Router>
             </div>
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,10 @@
 import {Button} from "@mantine/core";
-
+import Navbar from "../../components/Navbar/Navbar";
 
 function Home() {
     return (
     <div>
+        <Navbar/>
         <textarea cols="30" rows="15" style={{ width: '800px', height: '407px', resize : 'none'}} ></textarea>
 
         <Button> Executer</Button>
