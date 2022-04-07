c472f164dcf8320fae23955864aff19812062f0f
paulb <barrie.paul@protonmail.com>
2022-04-07 19:01:46 +0200

    fix: keycloak

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import {
     useMantineTheme,
 } from '@mantine/core';
 
+<<<<<<< HEAD
 import Home from "./pages/Home/Home";
 import Connexion from "./pages/Connexion/Connexion";
+=======
+import Home from "./containers/Home/Home";
+>>>>>>> fix: keycloak
 
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
+<<<<<<< HEAD
 import SignUp from "./pages/SignUp/SignUp";
 import Navbar from './components/Navbar/NavbarHead';
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./services/UserService";
 import { useKeycloak } from "@react-keycloak/web";
 import Welcome from './pages/Welcome/Welcome';
+=======
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import Welcome from './containers/Welcome/Welcome';
+import keycloak from './services/Keycloak';
+import PrivateRoute from './helpers/PrivateRoute';
+>>>>>>> fix: keycloak
 
 function App() {
     const theme = useMantineTheme();
@@ -25,10 +36,10 @@ function App() {
   return (
       <>
         <ReactKeycloakProvider authClient={keycloak}>
-            {keycloak.authenticated && <Navbar/>}
             <div>
                 <Router>
                     <Switch>
+<<<<<<< HEAD
                         {!keycloak.authenticated &&
                             <Route exact path='/' component={Welcome} />
                         }
@@ -38,6 +49,10 @@ function App() {
 
                         <Route exact path='/connexion' component={Connexion} />
                         <Route exact path='/signup' component={SignUp} />
+=======
+                        <Route  path='/home' component={Welcome} />
+                        <PrivateRoute  path='/' component={Home} />
+>>>>>>> fix: keycloak
                     </Switch>
                 </Router>
             </div>
