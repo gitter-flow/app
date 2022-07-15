b2dd71ddbfe3366361fa90b4653e114e8626d8eb
Yacine-Zegrour <yacinezegrour@gmail.com>
2022-07-15 17:45:54 +0200

    ajout dependances

--- src/components/Profile/JoinTeam.js
+++ src/components/Profile/JoinTeam.js
@@ -2,21 +2,21 @@ import * as React from "react";
 const JoinTeam = (props) => {
 
    function joinThisTeam() {
-     fetch(`${process.env.REACT_APP_API_URL}/team/join`, {
-      "headers": {
-        "accept": "application/json",
-        "authorization": `Bearer ${cookies["keycloaktoken"]}`,
-        "content-type": "application/json",
-      },
-      "body": `{\"userId\":\"${cookies["userId"]}\",\"teamId\":\"` + "8286c7c4-44c4-4845add3-a47a647526b2" + "\"}",
-      "method": "POST"
-    }).then(response => response.json())
-      .then(data => {
-        console.log(props.userId);
-        console.log(data);
-       // console.log(props.teamId);
-      })
-      //.catch(e => {console.log(e)})
+    //  fetch(`${process.env.REACT_APP_API_URL}/team/join`, {
+    //   "headers": {
+    //     "accept": "application/json",
+    //     "authorization": `Bearer ${cookies["keycloaktoken"]}`,
+    //     "content-type": "application/json",
+    //   },
+    //   "body": `{\"userId\":\"${cookies["userId"]}\",\"teamId\":\"` + "8286c7c4-44c4-4845add3-a47a647526b2" + "\"}",
+    //   "method": "POST"
+    // }).then(response => response.json())
+    //   .then(data => {
+    //     console.log(props.userId);
+    //     console.log(data);
+    //    // console.log(props.teamId);
+    //   })
+    //   //.catch(e => {console.log(e)})
 
   }
 
